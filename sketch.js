098bd082e8ffa97d1d7505cece1b1e4f3257d3a9
ArshAgarwal28 <61740528+ArshAgarwal28@users.noreply.github.com>
2020-12-01 10:04:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const {Engine, World, Bodies, Constraint, Body} = Matter;
+const { Engine, World, Bodies, Constraint, Body } = Matter;
 
 var engine, world;
 
@@ -7,53 +7,54 @@ var grid = 20;
 var w, h;
 var food;
 
-function setup(){
-  createCanvas(600, 600);
-  frameRate(8)
-  w = width/grid;
-  h = height/grid;
-  snake = new Snake();
+var canvas;
 
-  foodLocation();
+function setup() {
+	canvas = createCanvas(600, 600);
+	frameRate(8);
+	w = width / grid;
+	h = height / grid;
+	snake = new Snake();
+
+	foodLocation();
 }
 //start from food
 
 function draw() {
-  background(220);
-  scale(grid);
+	background(220);
+	scale(grid);
 
-  noStroke();
-  fill ("red");
-  rect(food.x, food.y, 1, 1) 
+	noStroke();
+	fill('red');
+	rect(food.x, food.y, 1, 1);
 
-  if (snake.eat(food)) {
-    foodLocation();
-  }
-  snake.update();
-  snake.show();  
+	if (snake.eat(food)) {
+		foodLocation();
+	}
+	snake.update();
+	snake.show();
 
-  if (snake.deathCheck()) {
-    background("red");
-    noLoop();
-  }
-
-   
+	if (snake.deathCheck()) {
+		background(rgb(255, 0, 0));
+		noLoop();
+	}
+	snake.changeSide();
 }
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    snake.setSpeed(0, -1)
-  } else if (keyCode === DOWN_ARROW) {
-    snake.setSpeed(0, 1)
-  } else if (keyCode === LEFT_ARROW) {
-    snake.setSpeed(-1, 0)
-  } else if (keyCode === RIGHT_ARROW) {
-    snake.setSpeed(1, 0)
-  }
+	if (keyCode === UP_ARROW && snake.ySpeed !== 1) {
+		snake.setSpeed(0, -1);
+	} else if (keyCode === DOWN_ARROW && snake.ySpeed !== -1) {
+		snake.setSpeed(0, 1);
+	} else if (keyCode === LEFT_ARROW && snake.xSpeed !== 1) {
+		snake.setSpeed(-1, 0);
+	} else if (keyCode === RIGHT_ARROW && snake.xSpeed !== -1) {
+		snake.setSpeed(1, 0);
+	}
 }
 
 function foodLocation() {
-  var x = floor(random(w));
-  var y = floor(random(h));
-  food = createVector(x, y)
+	var x = floor(random(w));
+	var y = floor(random(h));
+	food = createVector(x, y);
 }
